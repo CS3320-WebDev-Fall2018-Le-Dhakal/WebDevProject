480b0dbc62785e0e0f67a7057a71c21ef4fd509c
Sabal Dhakal <sabal.dhakal@g.austincc.edu>
2018-10-05 15:59:16 -0500

    Quote class

--- JavaClasses/Quote.java
+++ JavaClasses/Quote.java
@@ -1,33 +1,121 @@
-
 /**
  * Write a description of class Quote here.
  * 
- * @author (your name) 
+ * @Daniel Le and Sabal Dhakal
  * @version (a version number or a date)
  */
 public class Quote
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Quote
-     */
-    public Quote()
-    {
-        // initialise instance variables
-        x = 0;
+    private int clientId;
+    private double gallonsRequested;
+    private String deliveryDate;
+    private String requestDate;
+    private String deliveryLocation;  
+    private String deliveryContactName;
+    private String deliveryContactPhone;
+    private String deliveryContactEmail;
+    private double suggestedPrice;
+    private double totalAmountDue;
+    
+    
+    public Quote() { 	
     }
+    
+    public Quote(int clientId, double gallonsRequested, String deliveryDate, String requestDate,
+			String deliveryLocation, String deliveryContactName, String deliveryContactPhone,
+			String deliveryContactEmail, double suggestedPrice, double totalAmountDue) {
+		super();
+		this.clientId = clientId;
+		this.gallonsRequested = gallonsRequested;
+		this.deliveryDate = deliveryDate;
+		this.requestDate = requestDate;
+		this.deliveryLocation = deliveryLocation;
+		this.deliveryContactName = deliveryContactName;
+		this.deliveryContactPhone = deliveryContactPhone;
+		this.deliveryContactEmail = deliveryContactEmail;
+		this.suggestedPrice = suggestedPrice;
+		this.totalAmountDue = totalAmountDue;
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+	}
+
+	public int getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+
+	public double getGallonsRequested() {
+		return gallonsRequested;
+	}
+
+	public void setGallonsRequested(double gallonsRequested) {
+		this.gallonsRequested = gallonsRequested;
+	}
+
+	public String getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(String deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public String getRequestDate() {
+		return requestDate;
+	}
+
+	public void setRequestDate(String requestDate) {
+		this.requestDate = requestDate;
+	}
+
+	public String getDeliveryLocation() {
+		return deliveryLocation;
+	}
+
+	public void setDeliveryLocation(String deliveryLocation) {
+		this.deliveryLocation = deliveryLocation;
+	}
+
+	public String getDeliveryContactName() {
+		return deliveryContactName;
+	}
+
+	public void setDeliveryContactName(String deliveryContactName) {
+		this.deliveryContactName = deliveryContactName;
+	}
+
+	public String getDeliveryContactPhone() {
+		return deliveryContactPhone;
+	}
+
+	public void setDeliveryContactPhone(String deliveryContactPhone) {
+		this.deliveryContactPhone = deliveryContactPhone;
+	}
+
+	public String getDeliveryContactEmail() {
+		return deliveryContactEmail;
+	}
+
+	public void setDeliveryContactEmail(String deliveryContactEmail) {
+		this.deliveryContactEmail = deliveryContactEmail;
+	}
+
+	public double getSuggestedPrice() {
+		return suggestedPrice;
+	}
+
+	public void setSuggestedPrice(double suggestedPrice) {
+		this.suggestedPrice = suggestedPrice;
+	}
+
+	public double getTotalAmountDue() {
+		//Total amount will be generated here.
+		return totalAmountDue;
+	}
+
+	public void setTotalAmountDue(double totalAmountDue) {
+		this.totalAmountDue = totalAmountDue;
+	}
 }
